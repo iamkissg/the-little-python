62bcb90fa15556d6e7bec17179acb54d7f17313e
Engine <enginechen07@gmail.com>
2016-08-16 21:53:45 +0800

    refactor method: fill_walk

--- little-data-visualization/random_walk.py
+++ little-data-visualization/random_walk.py
@@ -27,15 +27,8 @@ class RandomWalk():
 
         while len(self.x_values) < self.num_points:
 
-            # obviously
-            x_direction = choice([1, -1])
-            x_distance = choice([0, 1, 2, 3, 4])
-            x_step = x_direction * x_distance
-
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
-
+            x_step = get_step()
+            y_step = get_step()
             # must move
             if x_step == 0 and y_step == 0:
                 continue
@@ -46,3 +39,9 @@ class RandomWalk():
 
             self.x_values.append(next_x)
             self.y_values.append(next_y)
+
+def get_step():
+    direction = choice([1, -1])
+    distance = choice([0, 1, 2, 3, 4])
+    return direction * distance
+
