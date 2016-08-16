3c910305d8293ea478e0a0c98b6db482c4adce0d
Engine <enginechen07@gmail.com>
2016-08-16 20:56:54 +0800

    v2.0

--- little-data-visualization/nw_visual.py
+++ little-data-visualization/nw_visual.py
@@ -9,8 +9,15 @@ __author__ = 'Engine'
 from matplotlib import pyplot as plt
 from random_walk import RandomWalk
 
-rw = RandomWalk()
-rw.fill_walk()
+while 1:
+    rw = RandomWalk()
+    rw.fill_walk()
 
-plt.scatter(rw.x_values, rw.y_values)
-plt.show()
+    plt.scatter(rw.x_values, rw.y_values)
+    # the program will stop at thi point
+    plt.show()
+
+    # only closing the window will ask user to make choice
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == "n":
+        break
