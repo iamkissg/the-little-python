10091e2e4089fec5c8db4c5b34c5de3dd050f81b
Engine <enginechen07@gmail.com>
2016-08-16 21:14:04 +0800

    more points and hide axes

--- little-data-visualization/nw_visual.py
+++ little-data-visualization/nw_visual.py
@@ -10,15 +10,20 @@ from matplotlib import pyplot as plt
 from random_walk import RandomWalk
 
 while 1:
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
     point_numers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=point_numers,
-                cmap=plt.cm.Reds, edgecolor="none", s=15)
+                cmap=plt.cm.Reds, edgecolor="none", s=1)
     plt.scatter(0, 0, c="blue", edgecolor="none", s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1],
                 c="green", edgecolor="none", s=100)
+
+    # hide axes
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
     # the program will stop at thi point
     plt.show()
 
