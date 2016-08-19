c913fddaece5098e6430ad87ce42e61a7d4b8680
Engine <enginechen07@gmail.com>
2016-08-19 21:59:04 +0800

    groupby pops, lack of some countries and areas

--- little-data-visualization/world_population/world_population.py
+++ little-data-visualization/world_population/world_population.py
@@ -20,6 +20,7 @@ with open(filename, "r") as f:
     # json.loads(s, encoding=None, cls=None, object_hook=None, parse_float=None, parse_int=None, parse_constant=None, object_pairs_hook=None, **kw)
     # deserialize s (a str instance containing a json document) to a Python object
 
+cc_populations, cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}, {}
 for pop_dict in pop_data:
     if pop_dict["Year"] == "2010":
         country_name = pop_dict["Country Name"]
@@ -29,7 +30,22 @@ for pop_dict in pop_data:
         population = int(float((pop_dict["Value"])))
         code = get_country_code(country_name)
         if code:
-            print(code + ": " + str(population))
-        else:
-            print("Error - " + country_name)
+            cc_populations[code] = population
+
+# 根据人口的不同量级再分组, 效果好很多
+for cc, pop in cc_populations.items():
+    if pop < 10000000:
+        cc_pops_1[cc] = pop
+    elif pop < 1000000000:
+        cc_pops_2[cc] = pop
+    else:
+        cc_pops_3[cc] = pop
+
+
+wm = pygal.maps.world.World()
+wm.title = "World Population in 2010, by Country"
+wm.add("0-10m", cc_pops_1)
+wm.add("10m-1b", cc_pops_2)
+wm.add(">1b", cc_pops_3)
+wm.render_to_file("world_population.svg")
 
